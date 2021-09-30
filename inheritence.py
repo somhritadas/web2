838f510c64de47e14a5b89b2766a3d5b64526e8b
somhritadas <91682557+somhritadas@users.noreply.github.com>
2021-09-30 21:02:27 +0530

    Add files via upload

---
+++ inheritence.py
@@ -0,0 +1,92 @@
+class Employee:
+    company="google"
+
+    def showDetails(self):
+        print("this is an employee")
+
+class Programmer(Employee):
+    Language=("python")
+
+    def getLanguage(self):
+        print(f"the language is {self.Language}")
+    
+    def showDetails(self):
+        print("this is a programmer")
+e=Employee()
+p=Programmer()
+p.showDetails()
+e.showDetails()
+print(p.company)
+
+
+class Employee:
+    company="visa"
+    ecode= 120
+    
+class Freelancer:
+    company="fiveer"
+    level=0
+    def upgradeLevel(self):
+        self.level=self.level + 1
+
+class Programmer(Employee,Freelancer):
+    name="Rohit"
+
+
+e=Employee()
+p=Programmer()
+p.upgradeLevel()
+print(p.level)
+print(p.company)
+
+#multiple inheritence
+class Employee:
+    company="visa"
+    ecode= 120
+    
+class Freelancer:
+    company="fiveer"
+    level=0
+    def upgradeLevel(self):
+        self.level=self.level + 1
+
+class Programmer(Employee,Freelancer):
+    name="Rohit"
+
+#multilevel inheritance
+
+class person:
+    country="india"
+
+    def takeBreath(self):
+        print("i am breathing")
+
+class employee(person):
+    company="honda"
+
+    def getSalary(self):
+        print(f"salary is {self.salary}")
+
+    def takeBreath(self):
+        print("i am luckily breathing ++")
+
+class programmer (employee):
+   company="fivver"
+    
+   def getSalary(self):
+       print("no salary for programmer")
+
+   def takeBreath(self):
+        print("i am breathing ..")
+
+p=programmer()
+e=employee()
+pr=person()
+pr.takeBreath()
+e.takeBreath()
+p.takeBreath()
+print(e.company)
+print(p.company)
+print(p.country)
+print(e.country)
+print(pr.country)
